4cdcf512bec253b6e7ed850a9046ada07c13b91f
Scott Finch <scott.m.finch@gmail.com>
2018-03-18 20:59:03 -0400

    Fixing up .gitignore issue

--- src/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyB1RS-AmwQZq7GyBerHHQZ3m_T7_wh7PTg",
-  authDomain: "joblist-fdce3.firebaseapp.com",
-  databaseURL: "https://joblist-fdce3.firebaseio.com",
-  projectId: "joblist-fdce3",
-  storageBucket: "joblist-fdce3.appspot.com",
-  messagingSenderId: "1000470810682"
-}
