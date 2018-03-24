6d09d5af88b0ca258c7b3e2c70952eb30b893899
Scott Finch <scott.m.finch@gmail.com>
2018-03-24 17:05:03 -0400

    New project saving to firebase

--- src/components/NewProject.vue
+++ src/components/NewProject.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h4>New Project</h4>
     <div class="row">
-      <form class="col s12">
+      <form @submit.prevent="saveProject" class="col s12">
         <div class="row">
           <div class="input-field col s1">
             <input type="text" v-model="jobNumber" required>
@@ -17,13 +17,15 @@
             <label>Description</label>
           </div>
         </div>
+        <button type="submit" class="btn">Create New Project</button>
+        <router-link to="/" class="btn grey right">Cancel</router-link>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-  import db from '../config/firebaseInit'
+  import db from '@/config/firebaseInit'
   
   export default {
     name: 'newProject',
@@ -44,7 +46,18 @@
     },
     methods: {
       saveProject () {
-
+        db.collection('jobs').add({
+          jobNumber: this.jobNumber,
+          customer: this.customer,
+          description: this.description
+        })
+        .then(docRef => {
+          console.log('Project added: ', docRef.Id)
+          this.$router.push('/')
+        })
+        .catch(error => {
+          console.error('Error adding project: ', error)
+        })
       }
     }
   }
